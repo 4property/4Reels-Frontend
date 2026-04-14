0489a645a6c1b0306433d81add84b0d08ee47ca4
Max <max.ferchland@gmx.de>
2026-04-14 16:57:20 +0100

    feat: add list of demovideos to reelchoice page

--- src/components/step-1/ReelChoice.jsx
+++ src/components/step-1/ReelChoice.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 
 export default function ReelChoice() {
-  return <div>Reelchoice</div>;
+  const videoData = [
+    // Just placeholder data for now, replace with actual API data when available
+    {src: "src/assets/video.mp4", title: "Video 1", id: 1},
+    {src: "src/assets/video.mp4", title: "Video 2", id: 2},
+    {src: "src/assets/video.mp4", title: "Video 3", id: 3},
+    {src: "src/assets/video.mp4", title: "Video 4", id: 4},
+    {src: "src/assets/video.mp4", title: "Video 5", id: 5},
+    {src: "src/assets/video.mp4", title: "Video 5", id: 5},
+    {src: "src/assets/video.mp4", title: "Video 5", id: 5},
+    {src: "src/assets/video.mp4", title: "Video 5", id: 5},
+    {src: "src/assets/video.mp4", title: "Video 5", id: 5},
+    
+  ]; // This will hold the video data fetched from the API
+
+  return (
+    <>
+      <div className="">Reelchoice</div>
+      <ul className="w-full h-auto flex  gap-4 flex-wrap">
+        {videoData.map((video) => (
+          <li className="w-40" key={video.id}>
+            <video src={video.src} title={video.title} />
+          </li>
+        ))}
+      </ul>
+    </>
+
+  );
 }
