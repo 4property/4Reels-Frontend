da0c2450556091e0151e2c647a3e2048f4391c52
Max <max.ferchland@gmx.de>
2026-04-15 10:47:10 +0100

    feat: changed the layout so that the div on the right is on the right height

--- src/components/step-1/ReelChoice.jsx
+++ src/components/step-1/ReelChoice.jsx
@@ -16,13 +16,13 @@ export default function ReelChoice() {
   ]; // This will hold the video data fetched from the API
 
   return (
-    <>
+    <div className="flex">
       <div className="w-3/5">
         {/* <div className="">Reelchoice</div> */}
         <h1>My Reels and Posters</h1>
         <h2>Scheduled videos and posters:</h2>
         <div className="w-full max-h-140 overflow-y-auto border border-slate-200 rounded-xl p-4 m-4">
-          <ul className="w-full h-auto flex gap-4 flex-wrap px-2 py-1">
+          <ul className="w-full h-auto flex gap-2 flex-wrap px-1 py-1">
             {videoData.map((video) => (
               <li className="w-40" key={video.id}>
                 <video src={video.src} title={video.title} />
@@ -33,11 +33,11 @@ export default function ReelChoice() {
       </div>
       <div className="w-2/5">
         <h2 className="p-2 m-4">Current Reel/Poster:</h2>
-        <div className="w-full h-auto border border-slate-200 rounded-xl p-2 m-4">
-          <video className="w-50" src="src/assets/video.mp4" title="Current Reel" controls />
+        <div className="w-full h-auto  p-2 m-4">
+          <video className="w-50 px-1 py-1" src="src/assets/video.mp4" title="Current Reel" controls />
         </div>
       </div>
-    </>
+    </div>
 
   );
 }
